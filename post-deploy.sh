fbc49364f99e3f0a3bce05b96a7e4689d30b580f
Mason Morris <66579883+MoosMase@users.noreply.github.com>
2025-03-24 11:49:20 -0500

    Update post-deploy.sh

--- post-deploy.sh
+++ post-deploy.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+#check app exists
+mkdir -p /var/www/html/whm-app
+
 # Naviagate to app directory
-cd /var/www/html/whm-app
+cd /var/www/html/whm-app || exit 1
+
+#check for pm2 (process manager)
+if ! command -V pm2 &> /dev/null
+then
+  echo "pm2 not found, installing..."
+  npm install -g pm2 || exit 1
+else
+  echo "pm2 is already installed"
+fi
 
 # Install dependencies
-npm install
+npm install || exit 1
 
 # Build next.js app
-npm run build
+npm run build || exit 1
 
 # Start app in production
-npm run start
+pm2 start /var/www/html/whm-app/dist/app.js --name "whm-app" || exit 1
